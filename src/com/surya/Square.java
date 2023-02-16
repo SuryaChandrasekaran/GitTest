308549709f86f890b69616ad2b3e66a344ddd231
SuryaChandrasekaran <csurya2460@gmail.com>
2023-02-16 18:49:28 +0530

    changed main test to square and square test.

---
+++ src/com/surya/Square.java
@@ -0,0 +1,14 @@
+package com.surya;
+
+public class Square {
+    int sides;
+
+    public Square() {
+        this.sides = 4;
+    }
+
+    public int getSides() {
+        return this.sides;
+    }
+
+}
